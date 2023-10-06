4ff887671d8249728e06a063edc8ce2b2054dbca
braga-gustavo <gustavo.b.moraes2@gmail.com>
2023-10-06 17:30:20 -0300

    initial commit and component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { SmallCardComponent } from './small-card/small-card.component';
+import { BigCardComponent } from './big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTitleComponent,
+    MenuBarComponent,
+    SmallCardComponent,
+    BigCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
